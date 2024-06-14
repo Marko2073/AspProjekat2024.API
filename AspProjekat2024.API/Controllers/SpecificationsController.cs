4b3e53beaa69e895aab1aa95839b9301520eb614
Marko2073 <95702488+Marko2073@users.noreply.github.com>
2024-06-14 13:24:17 +0200

    add controllers

---
+++ AspProjekat2024.API/Controllers/SpecificationsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspProjekat2024.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecificationsController : ControllerBase
+    {
+        // GET: api/<SpecificationsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SpecificationsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SpecificationsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SpecificationsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SpecificationsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
